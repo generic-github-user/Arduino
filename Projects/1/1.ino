01e2ad90878fd06e16a1c9e7f8a85d830ba7ec85
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2018-07-16 15:11:15 -0400

    Added code comments

--- Projects/1/1.ino
+++ Projects/1/1.ino
@@ -1,13 +1,19 @@
+// LED uses pin #13
 const int LED = 13;
 
-void setup()
-{
+// Setup
+void setup() {
   pinMode(LED, OUTPUT);
 }
 
+// Loop
 void loop() {
+  // Turn on LED
   digitalWrite(LED, HIGH);
+  // Wait 1 second
   delay(1000);
+  // Turn off LED
   digitalWrite(LED, LOW);
+  // Wait 1 second
   delay(1000);
 }
