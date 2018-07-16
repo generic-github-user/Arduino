6a0de253e425c9326231f1c2f78360a13cf69102
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2018-07-16 15:36:45 -0400

    Changed project name to "Blinking LED"

---
+++ Projects/Blinking LED/2/2.ino
@@ -0,0 +1,19 @@
+// LED uses pin #13
+const int LED = 13;
+
+// Setup
+void setup() {
+  pinMode(LED, OUTPUT);
+}
+
+// Loop
+void loop() {
+  // Turn on LED
+  digitalWrite(LED, HIGH);
+  // Wait 15 milliseconds
+  delay(15);
+  // Turn off LED
+  digitalWrite(LED, LOW);
+  // Wait 15 milliseconds
+  delay(15);
+}
