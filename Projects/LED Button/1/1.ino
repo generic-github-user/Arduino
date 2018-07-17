1b36a0f88ac9af03c0775bc83edfc4c33a12d68f
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2018-07-17 06:42:32 -0400

    Added code comments

--- Projects/LED Button/1/1.ino
+++ Projects/LED Button/1/1.ino
@@ -1,21 +1,30 @@
+// LED uses pin #13
 const int LED = 13;
+// Button uses pin #2
 const int BUTTON = 2;
-
+// Value of button (pressed or not)
 int val = 0;
 
+// Setup
 void setup() {
-  // put your setup code here, to run once:
+  // Set up the pin corresponding to the LED as an output
   pinMode(LED, OUTPUT);
+  // Set up the pin corresponding to the button as an input
   pinMode(BUTTON, INPUT);
 }
 
+// Loop
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Update button status variable (val)
   val = digitalRead(BUTTON);
+  // Detect if the button is pressed (val == HIGH)
   if (val == HIGH) {
+    // If the button is pressed, turn on the LED
     digitalWrite(LED, HIGH);
   }
+  // Detect if the button is not pressed (val != HIGH)
   else {
+    // If the button is not pressed, turn off the LED
     digitalWrite(LED, LOW);
   }
 }
