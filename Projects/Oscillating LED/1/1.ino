e337f87c5910f965c3c85277ba76067fe43df836
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2018-07-17 06:35:37 -0400

    Added Oscillating LED project

---
+++ Projects/Oscillating LED/1/1.ino
@@ -0,0 +1,18 @@
+// LED uses pin #9
+const int LED = 9;
+int time_ = 0;
+int sine = 0;
+
+// Setup
+void setup() {
+  pinMode(LED, OUTPUT);
+}
+
+// Loop
+void loop() {
+  sine = round((sin(time_ / 100) + 1) * (255 / 2));
+  analogWrite(LED, sine);
+  // Wait 1 millisecond
+  delay(1);
+  time_ ++;
+}
